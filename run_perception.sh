eaa756611fbef65080ce5ca09c6713c6f54d0c8e
Xingjian Liu <leoxliu@umich.edu>
2026-06-12 15:04:14 +0800

    fix: update OpenAI API key in run_perception.sh

--- run_perception.sh
+++ run_perception.sh
@@ -25,7 +25,7 @@ export MPLCONFIGDIR="${MPLCONFIGDIR:-/tmp/smartgrasp-matplotlib-${USER:-user}}"
 export MPLBACKEND=Agg
 export PYTHONUNBUFFERED=1
 export TOKENIZERS_PARALLELISM="${TOKENIZERS_PARALLELISM:-false}"
-export OPENAI_API_KEY='sk-0icgaaSMWa6ZBEmzKE960dC35DPmPuzUzN7hTGuFofOUCcHm'
+export OPENAI_API_KEY='sk-zOddAlSLsleWkmOYXR1iDjtWwC7a745r2fKjU5wEvbGOTwMO'
 export OPENAI_BASE_URL=https://www.highland-api.top/v1
 
 PYTHON="${PYTHON:-/home/qiuguanhe/miniconda3/envs/smartgrasp/bin/python}"
